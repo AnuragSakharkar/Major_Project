29b18ec902d801e100deecf0c86ee41b60453ed5
Sakharkar5604447 <5604447@sps-student.sk.ca>
2019-12-16 14:06:31 -0600

    Fix infinite loop in the menu update function

--- Projects/Pacman/Javascript/menu.js
+++ Projects/Pacman/Javascript/menu.js
@@ -2,14 +2,14 @@ function drawMenu()
 {
     push();
     rectMode(CENTER);
-    fill( 50);
+    fill(50);
     rect(windowWidth/3, windowHeight/2, scalar * 5, scalar * 5);
     rect(windowWidth * 2/3, windowHeight/2, scalar * 5, scalar * 5);
     pop();
 
     push();
     fill(255);
-    textSize(scalar/3)
+    textSize(scalar/3);
     text("CLASSIC PACMAN", windowWidth/(scalar/7.35), windowHeight/2);
     text("CUSTOM PACMAN", windowWidth/(scalar/18.35), windowHeight/2);
     pop();
@@ -24,7 +24,7 @@ function changeColor()
 
 function runMenu()
 {
-    runMenu();
+    drawMenu();
     changeColor();
 }
 
