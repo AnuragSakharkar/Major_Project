200a2ced20f0c92652176baa47ab64a348d15199
Sakharkar5604447 <5604447@sps-student.sk.ca>
2019-12-18 14:12:40 -0600

    Complete temporary menu

--- Projects/Pacman/Javascript/menu.js
+++ Projects/Pacman/Javascript/menu.js
@@ -1,30 +1,64 @@
+// Draw the buttons if in the menu mode
+
 function drawMenu()
 {
     push();
     rectMode(CENTER);
-    fill(50);
+    if (mouseX > (windowWidth/3 - scalar * 2.5) && mouseX < (windowWidth/3 + scalar * 2.5) && mouseY < (windowHeight/2 + scalar * 2.5) && mouseY > (windowHeight/2 - scalar * 2.5))
+    {
+        fill(255, 255, 255);
+    }
+    else
+    {
+        fill(75);
+    }
     rect(windowWidth/3, windowHeight/2, scalar * 5, scalar * 5);
-    rect(windowWidth * 2/3, windowHeight/2, scalar * 5, scalar * 5);
     pop();
 
     push();
-    fill(255);
+    rectMode(CENTER);
+    if (mouseX > (windowWidth * (2/3) - scalar * 2.5) && mouseX < (windowWidth * (2/3) + scalar * 2.5) && mouseY < (windowHeight/2 + scalar * 2.5) && mouseY > (windowHeight/2 - scalar * 2.5))
+    {
+        fill(255, 255, 255);
+    }
+    else
+    {
+        fill(75);
+    }
+    rect(windowWidth * (2/3), windowHeight/2, scalar * 5, scalar * 5);
+    pop();
+
+    push();
+    textAlign(CENTER);
+    fill(0);
     textSize(scalar/3);
-    text("CLASSIC PACMAN", windowWidth - scalar * 30, windowHeight/2);
-    text("CUSTOM PACMAN", windowWidth/(scalar), windowHeight/2);
+    text("CLASSIC PACMAN", windowWidth/3, windowHeight/2);
+    text("CUSTOM PACMAN", windowWidth * (2/3), windowHeight/2);
     pop();
 }
 
 
-function changeColor()
+
+// Change the color of the buttons if the mouse is above them
+
+function changeGameMode()
 {
-    
+    if (mouseX > (windowWidth/3 - scalar * 2.5) && mouseX < (windowWidth/3 + scalar * 2.5) && mouseY < (windowHeight/2 + scalar * 2.5) && mouseY > (windowHeight/2 - scalar * 2.5))
+    {
+        gameMode = "PACMAN";
+    }
+    else if (mouseX > (windowWidth * (2/3) - scalar * 2.5) && mouseX < (windowWidth * (2/3) + scalar * 2.5) && mouseY < (windowHeight/2 + scalar * 2.5) && mouseY > (windowHeight/2 - scalar * 2.5))
+    {
+        gameMode = "CUSTOM";
+    }
 }
 
+
+
+// "Update" function for the menu
 
 function runMenu()
 {
     drawMenu();
-    changeColor();
 }
 
