3d441fcbdb7f96d5e53685dcdf22d9bf89f067b3
AnuragSakharkar <anurag.kishore202@gmail.com>
2019-12-31 23:10:06 -0600

    Fix bug where the user coudl remove bounds in custom maps and break the entire game

--- Projects/Pacman/Javascript/custom.js
+++ Projects/Pacman/Javascript/custom.js
@@ -2,54 +2,57 @@
 
 function drawCustom()
 {
-  push();
-  strokeWeight(5);
-
-  for (let i = 0; i < maze.rows; i++)
+  if(gameMode === "CUSTOM")
   {
-    for (let j = 0; j < maze.cols; j++)
+    push();
+    strokeWeight(5);
+
+    for (let i = 0; i < maze.rows; i++)
     {
-      if (i < 3 || i === 24)
+      for (let j = 0; j < maze.cols; j++)
       {
-        stroke(0);
-      }
-      else
-      {
-        stroke(45, 45, 230);
-      }
+        if (i < 3 || i === 24)
+        {
+          stroke(0);
+        }
+        else
+        {
+          stroke(45, 45, 230);
+        }
 
-      if (maze.customGrid[i][j])
-      {
-        fill(60, 60, 255)
-      }
-      else
-      {
-        fill(0)
-      }
+        if (maze.customGrid[i][j])
+        {
+          fill(60, 60, 255)
+        }
+        else
+        {
+          fill(0)
+        }
 
-      square((j * scalar) + rectXOffset, (i * scalar) + rectYOffset, scalar + 1, 5);
+        square((j * scalar) + rectXOffset, (i * scalar) + rectYOffset, scalar + 1, 5);
 
+      }
     }
+    pop();
+
+    /*push();
+    strokeWeight(5);
+    stroke(45, 45, 230);
+    line(scalar * 11.9, windowHeight - (rectYOffset * 2.1), 26.8 * scalar, windowHeight - (rectYOffset * 2.1))
+    pop();*/
+
+    push();
+    fill(255);
+    rect(windowWidth - scalar * 5, windowHeight/2, scalar*3, scalar*3);
+    pop();
+
+    push();
+    fill(0);
+    textAlign(CENTER);
+    textSize(scalar/2)
+    text("DONE!", windowWidth - scalar * 5, windowHeight/2)
+    pop();
   }
-  pop();
-
-  push();
-  strokeWeight(5);
-  stroke(45, 45, 230);
-  line(scalar * 11.9, windowHeight - (rectYOffset * 2.1), 26.8 * scalar, windowHeight - (rectYOffset * 2.1))
-  pop();
-
-  push();
-  fill(255);
-  rect(windowWidth - scalar * 5, windowHeight/2, scalar*3, scalar*3);
-  pop();
-
-  push();
-  fill(0);
-  textAlign(CENTER);
-  textSize(scalar/2)
-  text("DONE!", windowWidth - scalar * 5, windowHeight/2)
-  pop();
 }
 
 
@@ -70,7 +73,7 @@ function changeMaze()
     foods.layDots();
   }
 
-  else if (yToChange > 2 && yToChange < 24 && xToChange != 0 && xToChange != 14)
+  else if (yToChange > 3 && yToChange < 23 && xToChange != 0 && xToChange != 14)
   {
     maze.customGrid[yToChange][xToChange] = !maze.customGrid[yToChange][xToChange]
   }
